1e533d4953ff11d943619efd3d8800556482227b
nitheshvar17 <nitheshvar17@gmail.com>
2026-01-29 08:24:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
-var map = L.map('map').setView([0, 0], 2);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-var marker = L.marker([0, 0]).addTo(map);
+let map;
 
-window.userLat = 0;
-window.userLng = 0;
+function showUserLocation() {
 
-if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(position => {
-        window.userLat = position.coords.latitude;
-        window.userLng = position.coords.longitude;
-        map.setView([window.userLat, window.userLng], 16);
-        marker.setLatLng([window.userLat, window.userLng]);
-    }, null, { enableHighAccuracy: true });
-}
+  document.getElementById("map").style.display = "block";
 
-function sendManualSOS() {
-    document.body.style.backgroundColor = "#d32f2f";
-    document.getElementById('status').innerText = "🚨 ALERT SENT TO POLICE & HOSPITAL";
+  if (!map) {
+    map = L.map('map').setView([0, 0], 15);
 
-    const name = document.getElementById('userName').value || "Manual User";
-    const phone = document.getElementById('userPhone').value || "No Phone";
-    const mapsLink = `https://www.google.com/maps?q=${window.userLat},${window.userLng}`;
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap'
+    }).addTo(map);
+  }
 
-    const formURL = "https://docs.google.com/forms/d/e/YOUR_FORM_ID/formResponse";
-    const formData = new FormData();
-    formData.append("entry.1896504086", name);     
-    formData.append("entry.1355037888", phone);    
-    formData.append("entry.1429143010", mapsLink); 
+  navigator.geolocation.getCurrentPosition(position => {
 
-    fetch(formURL, { method: "POST", body: formData, mode: "no-cors" })
-    .then(() => { alert("Location shared with Emergency Services."); });
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+
+    map.setView([lat, lon], 16);
+    L.marker([lat, lon]).addTo(map);
+
+  });
 }
